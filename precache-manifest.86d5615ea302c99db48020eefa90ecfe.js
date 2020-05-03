34c050552440006ea4d3029aa95f334ae1872bed
Hamzakam <mail.hamzakam@gmail.com>
2020-05-03 19:45:46 +0530

    Updates

---
+++ precache-manifest.86d5615ea302c99db48020eefa90ecfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1736381ddbc93a9446b862fcf8d73492",
+    "url": "/AniChara/index.html"
+  },
+  {
+    "revision": "e6eb45985e9cdab5a99d",
+    "url": "/AniChara/static/css/main.a38ac6e3.chunk.css"
+  },
+  {
+    "revision": "c4ef9528ba3e58a0db29",
+    "url": "/AniChara/static/js/2.452d8407.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AniChara/static/js/2.452d8407.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6eb45985e9cdab5a99d",
+    "url": "/AniChara/static/js/main.b5c5d074.chunk.js"
+  },
+  {
+    "revision": "0d70d5371894971e8b88",
+    "url": "/AniChara/static/js/runtime-main.4db57c9b.js"
+  }
+]);
